7059232b27199e0b3ad2c1f8fbb4448e1c5a51c7
Ali Bogatyrev <marksheyder06@gmail.com>
2022-08-06 18:15:53 +0300

    added index.html

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
